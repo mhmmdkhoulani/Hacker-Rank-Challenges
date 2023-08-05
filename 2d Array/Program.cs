7257afd1ff0fb722522d6fd7335665edeeea7a58
mo <mhmmdkholani@gmail.com>
2023-08-05 16:50:29 +0300

    Add project files.

---
+++ 2d Array/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace _2d_Array
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+
+            List<List<int>> arr = new List<List<int>>();
+
+            for (int i = 0; i < 6; i++)
+            {
+                arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+            }
+
+            int result = Result.hourglassSum(arr);
+
+            Console.WriteLine(result);
+
+            
+        }
+    }
+    class Result
+    {
+
+        /*
+         * Complete the 'hourglassSum' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+         */
+
+        public static int hourglassSum(List<List<int>> arr)
+        {
+            var result = -63;
+            var temp = 0;
+            int[][] arrayOfArrays = arr.Select(list => list.ToArray()).ToArray();
+            Console.WriteLine("array size" + arrayOfArrays.Length);
+            for(int i = 0; i < arrayOfArrays.Length - 2 ; i++)
+            {
+
+                for(int j =0 ; j < arrayOfArrays.Length - 2 ; j++)
+                {
+                    temp = arrayOfArrays[i][j] + arrayOfArrays[i][j + 1]  + arrayOfArrays[i][j + 2]
+                            + arrayOfArrays[i + 1][j + 1] +
+                           + arrayOfArrays[i + 2][j] + arrayOfArrays[i + 2][j + 1] + arrayOfArrays[i + 2][j + 2];
+                    if(temp > result)
+                    {
+                        result = temp;
+                    }
+                   
+                }
+                
+            }
+
+            return result;
+        }
+
+    }
+
+
+}
---
+++ Array Rotation/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Array_Rotation
+{
+    
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int n = Convert.ToInt32(firstMultipleInput[0]);
+
+            int d = Convert.ToInt32(firstMultipleInput[1]);
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            List<int> result = Result.rotateLeft(d, arr);
+
+            Console.WriteLine(String.Join(" ", result));
+
+        }
+    }
+
+    class Result
+    {
+
+        /*
+         * Complete the 'rotateLeft' function below.
+         *
+         * The function is expected to return an INTEGER_ARRAY.
+         * The function accepts following parameters:
+         *  1. INTEGER d
+         *  2. INTEGER_ARRAY arr
+         */
+
+        public static List<int> rotateLeft(int d, List<int> arr)
+        {
+            var elements = arr.Count;
+
+            int[] temp = new int[d];
+            var temp2 = new List<int>(arr);
+            for (int i = 0; i < temp.Length; i++)
+            {
+                temp[i] = arr[i];
+                
+            }
+            temp2.RemoveRange(0, d );
+            var shiftedArray = new int[elements];
+            temp2.CopyTo(shiftedArray, 0);
+            temp.CopyTo(shiftedArray, temp2.Count);
+            Console.WriteLine(shiftedArray);
+            return shiftedArray.ToList();
+        }
+
+    }
+}
